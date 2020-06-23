linuxdeepin/dtkwidget

feat: 修改 dsetttingdialg 的多级标题样式
修改标题栏的"设置"中间的多个标题的字体大小和宽度,以及间隔

Log: 修改"dsetttingdialg" 的样式, 变得更加美观

src/widgets/private/settings/content.cpp
@@ -33,6 +33,7 @@
 #include <DSuggestButton>
 #include <DPushButton>
 #include <DFontSizeManager>
+#include <DFrame>
 
 #include "dsettingswidgetfactory.h"
 
@@ -219,11 +220,15 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
         title->setTitle(trName);
         title->setProperty("key", groupKey);
         title->label()->setForegroundRole(QPalette::BrightText);
+        QSpacerItem *spaceItem = new QSpacerItem(0, 20,QSizePolicy::Minimum,QSizePolicy::Expanding);
+        d->contentLayout->setItem(d->contentLayout->rowCount(), QFormLayout::LabelRole, spaceItem);
 
         DFontSizeManager::instance()->bind(title, DFontSizeManager::T4, QFont::Medium);
         d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::LabelRole, title);
+        d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::SpanningRole, title);
         d->sortTitles.push_back(title);
         d->titles.insert(groupKey, title);
+
 
         for (auto subgroup : settings->group(groupKey)->childGroups()) {
             if (subgroup->isHidden()) {
@@ -242,9 +247,15 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
                 title->setProperty("key", subgroup->key());
                 title->setProperty("_d_dtk_group_key", current_groupKey);
 
+                QSpacerItem *spaceUp = new QSpacerItem(0, 6,QSizePolicy::Minimum,QSizePolicy::Expanding);
+                d->contentLayout->setItem(d->contentLayout->rowCount(), QFormLayout::LabelRole, spaceUp);
+
                 d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::LabelRole, title);
                 d->sortTitles.push_back(title);
                 d->titles.insert(subgroup->key(), title);
+
+                QSpacerItem *spaceDown = new QSpacerItem(0, 6,QSizePolicy::Minimum,QSizePolicy::Expanding);
+                d->contentLayout->setItem(d->contentLayout->rowCount(), QFormLayout::LabelRole, spaceDown);
             }
 
             for (auto option : subgroup->childOptions()) {
@@ -260,8 +271,26 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
                         if (widget.second)
                             label->setBuddy(widget.second);
                     }
+#if 1
+                    DFrame *frame = new DFrame();
+                    frame->setBackgroundRole(QPalette::Window);
+                    QHBoxLayout *hLay = new QHBoxLayout(frame);
+                    hLay->addWidget(widget.first);
+                    hLay->addWidget(widget.second);
+                    qDebug()<<"--->"<<widget.first<<widget.second;
+                    d->contentLayout->addRow(frame);
 
-                    d->contentLayout->addRow(widget.first, widget.second);
+#else
+                    DWidget *wid = new DWidget();
+                    wid->setStyleSheet("background-color: gray");
+                    QHBoxLayout *hLay = new QHBoxLayout(wid);
+                    hLay->addWidget(widget.first);
+                    hLay->addWidget(widget.second);
+                    qDebug()<<"--->"<<widget.first<<widget.second;
+                    d->contentLayout->addRow(wid);
+//                    d->contentLayout->addRow(widget.first, widget.second);
+
+#endif
 
                     if (widget.first) {
                         widget.first->setProperty("_d_dtk_group_key", current_subGroupKey);
@@ -273,9 +302,17 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
                 } else {
                     QWidget *widget = d->widgetFactory->createWidget(translateContext, option);
 
+//                    widget->setStyleSheet("background-color: yellow");
+
                     if (widget) {
                         widget->setProperty("_d_dtk_group_key", current_subGroupKey);
-                        d->contentLayout->addRow(widget);
+                        DFrame *frame = new DFrame();
+                        frame->setBackgroundRole(QPalette::Window);
+                        QHBoxLayout *hLay = new QHBoxLayout(frame);
+                        hLay->addWidget(widget);
+                        d->contentLayout->addRow(frame);
+//                        d->contentLayout->addRow(widget);
+
                     }
                 }
             }

ComixHe/dtk6widget

src/widgets/private/settings/content.cpp
@@ -33,6 +33,7 @@
 #include <DSuggestButton>
 #include <DPushButton>
 #include <DFontSizeManager>
+#include <DFrame>
 
 #include "dsettingswidgetfactory.h"
 
@@ -89,7 +90,7 @@ Content::Content(QWidget *parent)
     d->contentLayout->setRowWrapPolicy(QFormLayout::DontWrapRows);
     d->contentLayout->setLabelAlignment(Qt::AlignLeft);
     d->contentLayout->setFieldGrowthPolicy(QFormLayout::AllNonFixedFieldsGrow);
-    d->contentLayout->setContentsMargins(0, 0, d->contentArea->verticalScrollBar()->height(), 0);
+    d->contentLayout->setContentsMargins(0, 0, 10, 0);
     layout->addWidget(d->contentArea);
 
     d->contentArea->setWidget(d->contentFrame);
@@ -221,9 +222,15 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
         title->label()->setForegroundRole(QPalette::BrightText);
 
         DFontSizeManager::instance()->bind(title, DFontSizeManager::T4, QFont::Medium);
-        d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::LabelRole, title);
+
+        DWidget *widTile = new DWidget();
+        QHBoxLayout *hLayTile = new QHBoxLayout(widTile);
+        hLayTile->addWidget(title);
+        d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::SpanningRole, widTile);
+
         d->sortTitles.push_back(title);
         d->titles.insert(groupKey, title);
+        d->contentLayout->setSpacing(10);
 
         for (auto subgroup : settings->group(groupKey)->childGroups()) {
             if (subgroup->isHidden()) {
@@ -242,7 +249,12 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
                 title->setProperty("key", subgroup->key());
                 title->setProperty("_d_dtk_group_key", current_groupKey);
 
-                d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::LabelRole, title);
+                DWidget *wid = new DWidget();
+                QHBoxLayout *hLay = new QHBoxLayout(wid);
+                hLay->setContentsMargins(10, 0, 0, 0);
+                hLay->addWidget(title);
+                d->contentLayout->setWidget(d->contentLayout->rowCount(), QFormLayout::LabelRole, wid);
+
                 d->sortTitles.push_back(title);
                 d->titles.insert(subgroup->key(), title);
             }
@@ -261,7 +273,15 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
                             label->setBuddy(widget.second);
                     }
 
-                    d->contentLayout->addRow(widget.first, widget.second);
+                    DFrame *frame = new DFrame();
+                    frame->setLineWidth(0);
+                    frame->setBackgroundRole(DPalette::ItemBackground);
+                    QHBoxLayout *hLay = new QHBoxLayout(frame);
+                    QMargins margins = hLay->contentsMargins();
+                    hLay->setContentsMargins(15, margins.top(), margins.right(), margins.bottom());
+                    hLay->addWidget(widget.first, 2);
+                    hLay->addWidget(widget.second, 3);
+                    d->contentLayout->addRow(frame);
 
                     if (widget.first) {
                         widget.first->setProperty("_d_dtk_group_key", current_subGroupKey);
@@ -275,11 +295,21 @@ void Content::updateSettings(const QByteArray &translateContext, QPointer<DTK_CO
 
                     if (widget) {
                         widget->setProperty("_d_dtk_group_key", current_subGroupKey);
-                        d->contentLayout->addRow(widget);
+                        DFrame *frame = new DFrame();
+                        frame->setLineWidth(0);
+                        frame->setBackgroundRole(DPalette::ItemBackground);
+                        QHBoxLayout *hLay = new QHBoxLayout(frame);
+                        QMargins margins = hLay->contentsMargins();
+                        hLay->setContentsMargins(15, margins.top(), margins.right(), margins.bottom());
+                        hLay->addWidget(widget);
+                        d->contentLayout->addRow(frame);
+
                     }
                 }
             }
         }
+        QSpacerItem *spaceItem = new QSpacerItem(0, 20,QSizePolicy::Minimum,QSizePolicy::Expanding);
+        d->contentLayout->setItem(d->contentLayout->rowCount(), QFormLayout::LabelRole, spaceItem);
     }
 
     QWidget *box = new QWidget();